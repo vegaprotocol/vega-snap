d08bb6eb6cdd69ff7dc730ca69445826010e78f2
Jeremy Letang <me@jeremyletang.com>
2023-07-28 18:53:48 +0100

    chore: run linters
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,36 +1,44 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
-import rpc from "./node-rpc";
-import * as txs from "./transaction";
+import { vega } from '@vegaprotocol/protos';
+import { KeyPair } from '@vegaprotocol/crypto/cjs/keypair.cjs';
+import rpc from './node-rpc';
+import * as txs from './transaction';
 
-import { vega } from '@vegaprotocol/protos'
 // import * as InputData from '@vegaprotocol/protos/dist/vega/commands/v1/InputData/encode'
 // import * as Transaction from '@vegaprotocol/protos/dist/vega/commands/v1/Transaction/encode'
 // import { TX_VERSION_V3 } from '@vegaprotocol/protos/dist/vega/commands/v1/TxVersion'
 
-import { KeyPair } from '@vegaprotocol/crypto/cjs/keypair.cjs'
-
 const networks = new Map([
-  ['Mainnet', {
-    rest: [
-      'https://vega-mainnet-data.commodum.io',
-      'https://vega-data.nodes.guru:3008',
-      'https://vega-data.bharvest.io',
-      'https://datanode.vega.pathrocknetwork.org',
-      'https://vega.aurora-edge.com',
-      'https://darling.network'
-    ]
-  }],
-  ['Fairground', {
-    rest: [
-      "https://api.n06.testnet.vega.xyz",
-      "https://api.n07.testnet.vega.xyz"
-    ]
-  }]
-])
+  [
+    'Mainnet',
+    {
+      rest: [
+        'https://vega-mainnet-data.commodum.io',
+        'https://vega-data.nodes.guru:3008',
+        'https://vega-data.bharvest.io',
+        'https://datanode.vega.pathrocknetwork.org',
+        'https://vega.aurora-edge.com',
+        'https://darling.network',
+      ],
+    },
+  ],
+  [
+    'Fairground',
+    {
+      rest: [
+        'https://api.n06.testnet.vega.xyz',
+        'https://api.n07.testnet.vega.xyz',
+      ],
+    },
+  ],
+]);
 
-const DEFAULT_NETWORK = 'Fairground'
+const DEFAULT_NETWORK = 'Fairground';
 
-export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => {
+export const onRpcRequest: OnRpcRequestHandler = async ({
+  origin,
+  request,
+}) => {
   switch (request.method) {
     case 'client.list_keys':
       const entropy = await snap.request({
@@ -38,40 +46,47 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => 
         params: {
           path: ['m', "1789'", "0'", "0'"],
           curve: 'ed25519',
-        }
+        },
       });
 
-      const keyPair = await KeyPair.fromSeed(entropy.index, Buffer.from(entropy.privateKey.slice(2), 'hex'))
+      const keyPair = await KeyPair.fromSeed(
+        entropy.index,
+        Buffer.from(entropy.privateKey.slice(2), 'hex'),
+      );
 
       return {
-        keys: [{ name: 'Snap Key 1', publicKey: keyPair.publicKey.toString() }]
-      }
+        keys: [{ name: 'Snap Key 1', publicKey: keyPair.publicKey.toString() }],
+      };
 
     case 'client.get_chain_id':
-      var node = await rpc.findHealthyNode(networks.get(DEFAULT_NETWORK).rest.map(u => new URL(u)))
-      const res = await node.blockchainHeight()
+      var node = await rpc.findHealthyNode(
+        networks.get(DEFAULT_NETWORK).rest.map((u) => new URL(u)),
+      );
+      const res = await node.blockchainHeight();
 
-      return { chainID: res.chainId }
+      return { chainID: res.chainId };
 
     case 'client.send_transaction':
-	  const { publicKey, sendingMode, transaction } = request.params
-	  if (sendingMode != 'TYPE_SYNC' && sendingMode != 'TYPE_ASYNC') {
-	      throw new Error('Not a valid sending mode.')
-	  }
+      const { publicKey, sendingMode, transaction } = request.params;
+      if (sendingMode != 'TYPE_SYNC' && sendingMode != 'TYPE_ASYNC') {
+        throw new Error('Not a valid sending mode.');
+      }
 
-	  var proceed = await txs.review(origin, transaction)
-	  if (!proceed) {
-	      return serializeError('User rejected the transaction')
-	  }
+      var proceed = await txs.review(origin, transaction);
+      if (!proceed) {
+        return serializeError('User rejected the transaction');
+      }
 
-	  var node = await rpc.findHealthyNode(networks.get(DEFAULT_NETWORK).rest.map(u => new URL(u)))
+      var node = await rpc.findHealthyNode(
+        networks.get(DEFAULT_NETWORK).rest.map((u) => new URL(u)),
+      );
 
-	  return txs.send(node, transaction, sendingMode)
+      return txs.send(node, transaction, sendingMode);
 
     case 'client.connect_wallet':
     case 'client.disconnect_wallet':
     default:
-      throw new Error('Method not found.')
+      throw new Error('Method not found.');
   }
 };
 
@@ -80,5 +95,5 @@ const serializeError = (msg) => {
     error: {
       message: msg,
     },
-  }
-}
+  };
+};
--- packages/snap/src/transaction.ts
+++ packages/snap/src/transaction.ts
@@ -1,106 +1,103 @@
 import { panel, text, heading, divider, copyable } from '@metamask/snaps-ui';
-import { KeyPair } from '@vegaprotocol/crypto/cjs/keypair.cjs'
-import { toBase64, toHex } from '@vegaprotocol/crypto/cjs/buf.cjs'
-import { randomFill } from '@vegaprotocol/crypto/cjs/crypto.cjs'
-import { solve } from '@vegaprotocol/crypto/cjs/pow.cjs'
+import { KeyPair } from '@vegaprotocol/crypto/cjs/keypair.cjs';
+import { toBase64, toHex } from '@vegaprotocol/crypto/cjs/buf.cjs';
+import { randomFill } from '@vegaprotocol/crypto/cjs/crypto.cjs';
+import { solve } from '@vegaprotocol/crypto/cjs/pow.cjs';
 
 export const review = async (origin, transaction) => {
-    const header = TRANSACTION_TITLES[Object.keys(transaction)[0] as TransactionKeys]
+  const header =
+    TRANSACTION_TITLES[Object.keys(transaction)[0] as TransactionKeys];
 
-    return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'confirmation',
-            content: panel([
-		heading(`${header}`),
-		text(`Request from: **${origin}**`),
-		divider(),
-              copyable(`${JSON.stringify(transaction, null, 2)}`),
-          ]),
-        },
-    });
-}
+  return snap.request({
+    method: 'snap_dialog',
+    params: {
+      type: 'confirmation',
+      content: panel([
+        heading(`${header}`),
+        text(`Request from: **${origin}**`),
+        divider(),
+        copyable(`${JSON.stringify(transaction, null, 2)}`),
+      ]),
+    },
+  });
+};
 
 export const send = async (node, transaction, sendingMode) => {
-    const latestBlock = await node.blockchainHeight()
-    const tid = toHex(await randomFill(new Uint8Array(32)))
-    const chainId = latestBlock.chainId
+  const latestBlock = await node.blockchainHeight();
+  const tid = toHex(await randomFill(new Uint8Array(32)));
+  const { chainId } = latestBlock;
 
-    const pow = await solve(
-	latestBlock.spamPowDifficulty,
-	latestBlock.hash,
-	tid,
-    )
+  const pow = await solve(latestBlock.spamPowDifficulty, latestBlock.hash, tid);
 
-      const entropy = await snap.request({
-        method: 'snap_getBip32Entropy',
-        params: {
-          path: ['m', "1789'", "0'", "0'"],
-          curve: 'ed25519',
-        }
-      });
+  const entropy = await snap.request({
+    method: 'snap_getBip32Entropy',
+    params: {
+      path: ['m', "1789'", "0'", "0'"],
+      curve: 'ed25519',
+    },
+  });
 
-    const keyPair = await KeyPair.fromSeed(entropy.index, Buffer.from(entropy.privateKey.slice(2), 'hex'))
+  const keyPair = await KeyPair.fromSeed(
+    entropy.index,
+    Buffer.from(entropy.privateKey.slice(2), 'hex'),
+  );
 
-    const nonce = new DataView(await randomFill(new Uint8Array(8)).buffer).getBigUint64(0, false)
+  const nonce = new DataView(
+    await randomFill(new Uint8Array(8)).buffer,
+  ).getBigUint64(0, false);
 
+  // TODO(): remove for the real stuff...
+  const inputData = new Buffer('inputData');
+  // const inputData = vega.commands.v1.InputData.encode({
+  // 	blockHeight: BigInt(latestBlock.height),
+  // 	nonce,
+  // 	command: transaction
+  // })
 
-    // TODO(): remove for the real stuff...
-    const inputData = new Buffer('inputData')
-    // const inputData = vega.commands.v1.InputData.encode({
-    // 	blockHeight: BigInt(latestBlock.height),
-    // 	nonce,
-    // 	command: transaction
-    // })
+  const txData = {
+    inputData,
+    signature: {
+      value: toHex(await keyPair.sign(inputData, chainId)),
+      algo: keyPair.algorithm.name,
+      version: keyPair.algorithm.version,
+    },
+    from: {
+      pubKey: keyPair.publicKey.toString(),
+    },
+    version: 3,
+    pow,
+  };
 
+  // TODO(): remove for the real stuff
+  const tx = new Buffer('tx');
+  // const tx = Transaction.encode(txData)
 
-    const txData = {
-	inputData,
-	signature: {
-	    value: toHex(await keyPair.sign(inputData, chainId)),
-	    algo: keyPair.algorithm.name,
-	    version: keyPair.algorithm.version
-	},
-	from: {
-	    pubKey: keyPair.publicKey.toString()
-	},
-	version: 3,
-	pow
-    }
+  const txJSON = {
+    inputData: toBase64(inputData),
+    signature: {
+      value: txData.signature.value,
+      algo: txData.signature.algo,
+      version: txData.signature.version,
+    },
+    from: {
+      pubKey: txData.from.pubKey,
+    },
+    version: txData.version,
+    pow: {
+      tid: toHex(tid),
+      nonce: pow.nonce.toString(),
+    },
+  };
 
-    // TODO(): remove for the real stuff
-    const tx = new Buffer('tx')
-    // const tx = Transaction.encode(txData)
+  const sentAt = new Date().toISOString();
+  const res = await node.submitRawTransaction(toBase64(tx), sendingMode);
 
-    const txJSON = {
-	inputData: toBase64(inputData),
-	signature: {
-	    value: txData.signature.value,
-	    algo: txData.signature.algo,
-	    version: txData.signature.version
-	},
-	from: {
-	    pubKey: txData.from.pubKey
-	},
-	version: txData.version,
-	pow: {
-	    tid: toHex(tid),
-	    nonce: pow.nonce.toString()
-	}
-    }
-
-    const sentAt = new Date().toISOString()
-    const res = await node.submitRawTransaction(
-	toBase64(tx),
-	sendingMode
-    )
-
-    return {
-	sentAt,
-	transactionHash: res.txHash,
-	transaction: txJSON
-    }
-}
+  return {
+    sentAt,
+    transactionHash: res.txHash,
+    transaction: txJSON,
+  };
+};
 
 enum TransactionKeys {
   UNKNOWN = 'unknown',
@@ -123,7 +120,7 @@ enum TransactionKeys {
   TRANSFER = 'transfer',
   CANCEL_TRANSFER = 'cancelTransfer',
   KEY_ROTATE_SUBMISSION = 'keyRotateSubmission',
-  ETHEREUM_KEY_ROTATE_SUBMISSION = 'ethereumKeyRotateSubmission'
+  ETHEREUM_KEY_ROTATE_SUBMISSION = 'ethereumKeyRotateSubmission',
 }
 
 const TRANSACTION_TITLES: Record<TransactionKeys, string> = {
@@ -134,8 +131,10 @@ const TRANSACTION_TITLES: Record<TransactionKeys, string> = {
   [TransactionKeys.VOTE_SUBMISSION]: 'Vote submission',
   [TransactionKeys.WITHDRAW_SUBMISSION]: 'Withdraw submission',
   [TransactionKeys.LIQUIDTY_PROVISION_SUBMISSION]: 'Liquidity provision',
-  [TransactionKeys.LIQUIDTY_PROVISION_CANCELLATION]: 'Liquidity provision cancellation',
-  [TransactionKeys.LIQUIDITY_PROVISION_AMENDMENT]: 'Liquidity provision amendment',
+  [TransactionKeys.LIQUIDTY_PROVISION_CANCELLATION]:
+    'Liquidity provision cancellation',
+  [TransactionKeys.LIQUIDITY_PROVISION_AMENDMENT]:
+    'Liquidity provision amendment',
   [TransactionKeys.PROPOSAL_SUBMISSION]: 'Proposal submission',
   [TransactionKeys.ANNOUNCE_NODE]: 'Announce node',
   [TransactionKeys.NODE_VOTE]: 'Node vote',
@@ -147,5 +146,6 @@ const TRANSACTION_TITLES: Record<TransactionKeys, string> = {
   [TransactionKeys.TRANSFER]: 'Transfer',
   [TransactionKeys.CANCEL_TRANSFER]: 'Cancel transfer',
   [TransactionKeys.KEY_ROTATE_SUBMISSION]: 'Key rotation submission',
-  [TransactionKeys.ETHEREUM_KEY_ROTATE_SUBMISSION]: 'Ethereum key rotation submission'
-}
+  [TransactionKeys.ETHEREUM_KEY_ROTATE_SUBMISSION]:
+    'Ethereum key rotation submission',
+};
