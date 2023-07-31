40db094b7b2d914e5f60c7faffc21bc909634645
Jeremy Letang <me@jeremyletang.com>
2023-07-31 16:30:17 +0100

    feat: some UI improvements
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>

--- packages/snap/src/ui.ts
+++ packages/snap/src/ui.ts
@@ -9,13 +9,11 @@ import { panel, heading, text, divider, copyable } from '@metamask/snaps-ui';
  * @returns `true` if the user approves the transaction, `false` otherwise.
  */
 export async function reviewTransaction(origin: string, transaction: any) {
-  const transactionContent = transaction[Object.keys(transaction)[0]];
-
   const content = panel([
     heading(transactionTitle(transaction)),
     text(`Request from: **${origin}**`),
     divider(),
-    ...prettyPrint(transactionContent),
+      ...prettyPrintTx(transaction, text),
     divider(),
     text('Raw transaction:'),
     copyable(JSON.stringify(transaction, null, 2)),
@@ -53,6 +51,211 @@ export async function debug(obj: any) {
       content,
     },
   });
+}
+
+function indentText(t: string) {
+    const indent = '&nbsp;&nbsp;&nbsp;&nbsp;'
+    return text(`${indent}${t}`)
+}
+
+function minimiseId(id: string) {
+    if (id.length >= 12) {
+	return id.slice(0, 6) + '…' + id.slice(id.length - 6, id.length);
+    }
+    return id;
+}
+
+function prettyPrintTx(tx: any, textFn: any) {
+  const keys = Object.keys(tx);
+  const txContent = tx[Object.keys(tx)[0]];
+
+  if (keys.length !== 1) {
+    throw new Error('Invalid transaction');
+  }
+
+    switch (keys[0]) {
+	case 'batchMarketInstructions':
+	    return prettyPrintBatchMarketInstructions(txContent);
+	case 'orderSubmission':
+	    return prettyPrintOrderSubmission(txContent, textFn);
+	case 'orderCancellation':
+	    return prettyPrintCancelOrder(txContent, textFn);
+	case 'orderAmendment':
+	    return prettyPrintOrderAmendment(txContent, textFn);
+	case 'withdrawSubmission':
+	    prettyPrintWithdrawSubmission(txContent, textFn);
+	case 'transfer':
+            return prettyPrint(txContent);
+	default:
+	    return prettyPrint(txContent);
+  }
+}
+
+function prettyPrintWithdrawSubmission(tx: any, textFn: any) {
+    let elms = [];
+    return elms;
+}
+
+function prettyPrintOrderSubmission(tx: any, textFn: any) {
+    let elms = [];
+    let isLimit = tx.price !== undefined && tx.price !== null && tx.price !== '';
+    let side = tx.side == 'TYPE_BUY' ? 'buy' : 'sell';
+
+    if (tx.peggedOrder) {
+	elms.push(textFn(`Pegged Limit ${side} - ${getTimeInForce(tx.timeInForce)} ${tx.size} @ ${getPeggedReference(tx.peggedOrder.reference)}+${tx.peggedOrder.offset}`))
+    } else if (isLimit) {
+	elms.push(textFn(`Limit ${side} - ${getTimeInForce(tx.timeInForce)} ${tx.size} @ ${tx.price}`))
+    } else {
+	elms.push(textFn(`Market ${side} - ${getTimeInForce(tx.timeInForce)} ${tx.size}`))
+    }
+
+    let marketId = minimiseId(tx.marketId);
+    elms.push(textFn(`**market id**: ${marketId}`));
+
+    if (tx.expiresAt && tx.expiresAt > 0) {
+	elms.push(textFn(`**expires at**: ${marketId}`));
+    }
+
+    if (tx.postOnly) {
+	elms.push(textFn(`**post only**: yes`));
+    }
+    if (tx.reduceOnly) {
+	elms.push(textFn(`**reduce only**: yes`));
+    }
+
+    if (tx.icebergOpts) {
+	elms.push(textFn(`**iceberg peak size**: ${tx.icebergOpts.peakSize}`));
+	elms.push(textFn(`**iceberg minimum visible size**: ${tx.icebergOpts.minimumVisibleSize}`));
+    }
+
+    return elms;
+}
+
+function prettyPrintOrderAmendment(tx: any, textFn: any) {
+    let elms = [
+	textFn(`**order id**: ${minimiseId(tx.orderId)}`),
+	textFn(`**market id**: ${minimiseId(tx.marketId)}`),
+    ];
+
+    if (tx.price !== undefined && tx.price !== null && tx.price !== '') {
+	elms.push(textFn(`**price**: ${tx.price}`))
+    }
+    if (tx.sizeDelta !== undefined && tx.sizeDelta !== null && tx.sizeDelta !== 0) {
+	if (tx.sizeDelta > 0) {
+	    elms.push(textFn(`**size delta**: +${tx.sizeDelta}`))
+	} else {
+	    elms.push(textFn(`**size delta**: ${tx.sizeDelta}`))
+	}
+    }
+    if (tx.expiresAt !== undefined && tx.expiresAt !== null && tx.expiresAt !== 0) {
+	elms.push(textFn(`**expires at**: ${tx.expiresAt}`))
+    }
+    if (tx.timeInForce !== undefined && tx.timeInForce !== null && tx.timeInForce !== '') {
+	elms.push(textFn(`**time in force**: ${getTimeInForce(tx.timeInForce)}`))
+    }
+    if (tx.peggedReference !== undefined && tx.peggedReference !== null && tx.peggededReference !== '') {
+	elms.push(textFn(`**pegged reference**: ${getPeggedReference(tx.peggedReference)}`))
+    }
+    if (tx.peggedOffset !== undefined && tx.peggedOffset !== null && tx.peggededOffset !== '') {
+	elms.push(textFn(`**pegged offset**: ${tx.peggedOffset}`))
+    }
+
+    return elms;
+}
+
+function getPeggedReference(ref: string) {
+    switch (ref) {
+	case 'PEGGED_REFERENCE_MID':
+	    return 'Mid';
+	case 'PEGGED_REFERENCE_BEST_BID':
+	    return 'Bid';
+	case 'PEGGED_REFERENCE_BEST_ASK':
+	    return 'Ask';
+    }
+}
+
+function getTimeInForce(tif: string) {
+    switch (tif) {
+	case 'TIME_IN_FORCE_GTC':
+	    return 'GTC';
+	case 'TIME_IN_FORCE_GTT':
+	    return 'GTT';
+	case 'TIME_IN_FORCE_IOC':
+	    return 'IOC';
+	case 'TIME_IN_FORCE_FOK':
+	    return 'FOK';
+	case 'TIME_IN_FORCE_GFA':
+	    return 'GFA';
+	case 'TIME_IN_FORCE_GFN':
+	    return 'GFN';
+    }
+}
+
+function prettyPrintBatchMarketInstructions(tx: any) {
+    const elms = [];
+    let addDivider = false;
+
+    if (tx.cancellations && tx.cancellations.length > 0) {
+	elms.push(text(`**Order Cancellations:**`));
+	let i = 0;
+	for (const c of tx.cancellations.values()) {
+	    elms.push(text(`__${i}:__`));
+	    elms.push(...prettyPrintTx({orderCancellation: c}, indentText));
+	    i += 1;
+	}
+	addDivider = true;
+    }
+
+    if (tx.amendments && tx.amendments.length > 0) {
+	if (addDivider) {
+	    elms.push(divider());
+	}
+	elms.push(text(`**Order Amendments:**`));
+	let i = 0;
+	for (const c of tx.amendments.values()) {
+	    elms.push(text(`__${i}:__`));
+	    elms.push(...prettyPrintTx({orderAmendment: c}, indentText));
+	    i += 1;
+	}
+	addDivider = true;
+    }
+
+    if (tx.submissions && tx.submissions.length > 0) {
+	if (addDivider) {
+	    elms.push(divider());
+	}
+	elms.push(text(`**Order Submissions:**`));
+	let i = 0;
+	for (const c of tx.submissions.values()) {
+	    elms.push(text(`__${i}:__`));
+	    elms.push(...prettyPrintTx({orderSubmission: c}, indentText));
+	    i += 1;
+	}
+    }
+
+    return elms;
+}
+
+function prettyPrintCancelOrder(tx: any, textFn: any) {
+    const hasOrderId = tx.orderId !== undefined && tx.orderId !== null && tx.orderId != '';
+    const hasMarketId = tx.marketId !== undefined && tx.marketId !== null && tx.marketId != '';
+
+    if (hasOrderId && hasMarketId) {
+	return [
+	    textFn(`Cancel order`),
+	    textFn(`**order id**: ${minimiseId(tx.orderId)}`),
+	    textFn(`**market id**: ${minimiseId(tx.marketId)}`),
+	]
+    } else if (hasOrderId) {
+	return [textFn(`Cancel order ${minimiseId(tx.orderId)}`)]
+    } else if (hasMarketId) {
+	return [
+	    textFn(`Cancel all orders on market`),
+	    textFn(`**market id**: ${minimiseId(tx.marketId)}`),
+	]
+    } else {
+	return [textFn(`Cancel all orders from all markets`)]
+    }
 }
 
 /**
