ce52927afb10923ff1b1bba8218d442775a06451
Emil Bay <github@tixz.dk>
2023-07-31 10:39:10 +0200

    chore: remove unused types file

--- packages/snap/src/types.ts
+++
@@ -1,9 +0,0 @@
-export type RpcListKeys = {};
-
-export type RpcGetChainId = {};
-
-export type RpcSendTransaction = {
-  transaction: Transaction;
-  sendingMode: 'TYPE_SYNC' | 'TYPE_ASYNC';
-  publicKey: string;
-};
