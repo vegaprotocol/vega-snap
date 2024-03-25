300c58de43b2fe729d994d5f3feff89f4751e639
Dexter <dexter@vega.xyz>
2024-03-25 14:51:41 +0530

    refactor: turn switch into a map for factory pattern

--- packages/snap/src/transaction-ui/pretty-print-tx.ts
+++ packages/snap/src/transaction-ui/pretty-print-tx.ts
@@ -1,4 +1,6 @@
+import { text } from '@metamask/snaps-sdk';
 import { invalidParameters } from '../errors';
+import { EnrichmentData, VegaTransaction } from '../types';
 import {
   prettyPrintBatchMarketInstructions,
   prettyPrintOrderSubmission,
@@ -16,6 +18,31 @@ import {
   prettyPrintUpdatePartyProfile,
   prettyPrint,
 } from './commands';
+import { getFormatNumber } from './utils';
+
+type PrettyPrintTxFn = (
+  tx: VegaTransaction,
+  textFn: typeof text,
+  enrichmentData: EnrichmentData,
+  formatNumber: ReturnType<typeof getFormatNumber>,
+) => any[];
+
+const TRANSACTION_MAP: Record<string, PrettyPrintTxFn> = {
+  batchMarketInstructions: prettyPrintBatchMarketInstructions,
+  orderSubmission: prettyPrintOrderSubmission,
+  orderCancellation: prettyPrintCancelOrder,
+  orderAmendment: prettyPrintOrderAmendment,
+  stopOrdersSubmission: prettyPrintStopOrdersSubmission,
+  stopOrdersCancellation: prettyPrintStopOrdersCancellation,
+  withdrawSubmission: prettyPrintWithdrawSubmission,
+  transfer: prettyPrintTransferFunds,
+  updateMarginMode: prettyPrintUpdateMarginMode,
+  createReferralSet: prettyPrintCreateReferralSet,
+  updateReferralSet: prettyPrintUpdateReferralSet,
+  applyReferralCode: prettyPrintApplyReferralCode,
+  joinTeam: prettyPrintJoinTeam,
+  updatePartyProfile: prettyPrintUpdatePartyProfile,
+};
 
 /**
  * Pretty prints a transaction depending of its type.
@@ -23,14 +50,14 @@ import {
  * @param tx - The transaction to be pretty printed.
  * @param textFn - The text function to be use for rendering.
  * @param enrichmentData - Data used to enrich the transaction data to make it more human readable.
- * @param formatNumber
+ * @param formatNumber - Function to format numbers based on the user's locale.
  * @returns List of snap-ui elements.
  */
 export function prettyPrintTx(
-  tx: any,
-  textFn: any,
-  enrichmentData: any,
-  formatNumber: (number: string) => string,
+  tx: VegaTransaction,
+  textFn: typeof text,
+  enrichmentData: EnrichmentData,
+  formatNumber: ReturnType<typeof getFormatNumber>,
 ) {
   const keys = Object.keys(tx);
   console.log(formatNumber('10000000'));
@@ -40,45 +67,9 @@ export function prettyPrintTx(
   }
 
   const txContent = tx[keys[0]];
-
-  switch (keys[0]) {
-    case 'batchMarketInstructions':
-      return prettyPrintBatchMarketInstructions(txContent, enrichmentData);
-    case 'orderSubmission':
-      return prettyPrintOrderSubmission(txContent, textFn, enrichmentData);
-    case 'orderCancellation':
-      return prettyPrintCancelOrder(txContent, textFn, enrichmentData);
-    case 'orderAmendment':
-      return prettyPrintOrderAmendment(txContent, textFn, enrichmentData);
-    case 'stopOrdersSubmission':
-      return prettyPrintStopOrdersSubmission(txContent, textFn, enrichmentData);
-    case 'stopOrdersCancellation':
-      return prettyPrintStopOrdersCancellation(
-        txContent,
-        textFn,
-        enrichmentData,
-      );
-
-    case 'withdrawSubmission':
-      return prettyPrintWithdrawSubmission(txContent, textFn, enrichmentData);
-    case 'transfer':
-      return prettyPrintTransferFunds(txContent, textFn, enrichmentData);
-
-    // TODO: do we care about this one?
-    case 'updateMarginMode':
-      return prettyPrintUpdateMarginMode(txContent, textFn);
-
-    case 'createReferralSet':
-      return prettyPrintCreateReferralSet(txContent, textFn);
-    case 'updateReferralSet':
-      return prettyPrintUpdateReferralSet(txContent, textFn);
-    case 'applyReferralCode':
-      return prettyPrintApplyReferralCode(txContent, textFn);
-    case 'joinTeam':
-      return prettyPrintJoinTeam(txContent, textFn);
-    case 'updatePartyProfile':
-      return prettyPrintUpdatePartyProfile(txContent, textFn);
-    default:
-      return prettyPrint(txContent);
+  const renderTx = TRANSACTION_MAP[keys[0]];
+  if (renderTx) {
+    return renderTx(txContent, textFn, enrichmentData, formatNumber);
   }
+  return prettyPrint(txContent);
 }
